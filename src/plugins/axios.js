594a14cc69d25e35082a815151c571295eda79f3
Stéphane legouffe <slegouffe@gmail.com>
2018-08-14 16:36:30 +0200

    Fix "build" API url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.API_URL || `${window.location.hostname}:3001`,
 });
 
 export default ({ Vue }) => {
