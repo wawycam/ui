c553da860cfed9fbf013e4ec0dd8f472ba56c986
Stéphane legouffe <slegouffe@gmail.com>
2018-08-14 16:43:39 +0200

    Fix missing protocol scheme

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL || `${window.location.hostname}:3001`,
+  baseURL: process.env.API_URL || `http://${window.location.hostname}:3001`,
 });
 
 export default ({ Vue }) => {
+  console.log(window.location.hostname);
   Vue.use(VueAxios, axiosInstance);
 };
